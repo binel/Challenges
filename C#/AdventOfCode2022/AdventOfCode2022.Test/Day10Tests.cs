5d80c372e9140bd87cc0dcd737cfc70da6478084
Ben Nelson <binelson13@gmail.com>
2023-03-23 21:11:01 -0500

    AoC 2022 Day 10 part 1

---
+++ C#/AdventOfCode2022/AdventOfCode2022.Test/Day10Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Test
+{
+    internal class Day10Tests : BaseDayTests
+    {
+        internal override BaseDay Day => new Day10();
+
+        internal override string Part1TrainingSolution => "13140";
+
+        internal override string Part1Solution => "14360";
+
+        internal override string Part2TrainingSolution => "0";
+
+        internal override string Part2Solution => "0";
+
+        [Test]
+        public void SmallProgram()
+        {
+            Day10.CPU cpu = new Day10.CPU();
+            string[] program = new string[3];
+            program[0] = "noop";
+            program[1] = "addx 3";
+            program[2] = "addx -5";
+
+            cpu.Program = program;
+
+            cpu.Tick();
+            Assert.That(cpu.Register, Is.EqualTo(1));
+            cpu.Tick();
+            Assert.That(cpu.Register, Is.EqualTo(1));
+            cpu.Tick();
+            Assert.That(cpu.Register, Is.EqualTo(4));
+            cpu.Tick();
+            cpu.Tick();
+            Assert.That(cpu.Register, Is.EqualTo(-1));
+            cpu.Tick();
+            Assert.That(cpu.ProgramComplete, Is.True);
+        }
+
+        [TestCase(20, ExpectedResult = 420)]
+        [TestCase(60, ExpectedResult = 1140)]
+        [TestCase(100, ExpectedResult = 1800)]
+        [TestCase(140, ExpectedResult = 2940)]
+        [TestCase(180, ExpectedResult = 2880)]
+        [TestCase(220, ExpectedResult = 3960)]
+        public int Part1Training_StrengthAtCycle(int cycle)
+        {
+            Day10 d = new Day10();
+            Day10.CPU cpu = new Day10.CPU();
+            cpu.Program = d.GetInput(true);
+
+            while(cpu.Cycle != cycle)
+            {
+                cpu.Tick();
+            }
+
+            return cpu.SignalStrength;
+        }
+    }
+}
---
+++ C#/AdventOfCode2022/AdventOfCode2022/Day10.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Day10 : BaseDay
+    {
+        public override int DayNumber { get; set; } = 10;
+
+        public List<int> InterestingCycles = new List<int> { 20, 60, 100, 140, 180, 220 };
+
+        public override string PuzzlePart1(bool training)
+        {
+            CPU cpu = new CPU();
+            cpu.Program = GetInput(training);
+            int sumStrength = 0;
+            while(!cpu.ProgramComplete)
+            {
+                cpu.Tick();
+                if (InterestingCycles.Contains(cpu.Cycle))
+                {
+                    sumStrength += cpu.SignalStrength;
+                }
+            }
+
+            return sumStrength.ToString();
+        }
+
+        public override string PuzzlePart2(bool training)
+        {
+            return "Not Done";
+        }
+
+        public class CPU
+        {
+            public int Cycle { get; set; } = 0;
+
+            public int Register { get; set; } = 1;
+
+            public int MidCycleRegisterValue { get; set; } = 1;
+
+            public string CurrentOperation { get; set; } 
+
+            public bool MidOperation { get; set; }
+
+            public string[] Program { get; set; }
+
+            public int InstructionPointer { get; set; } = -1;
+
+            public bool ProgramComplete { get; set; }
+
+            public int SignalStrength => MidCycleRegisterValue * Cycle;
+
+            public void Tick()
+            {
+                Cycle++;
+                MidCycleRegisterValue = Register;
+                if (!MidOperation)
+                {
+                    InstructionPointer++;
+
+                    if (InstructionPointer >= Program.Length)
+                    {
+                        ProgramComplete = true;
+                        return;
+                    }
+
+                    var input = Program[InstructionPointer];
+
+                    if (input == "noop")
+                    {
+                        return;
+                    }
+                    MidOperation = true;
+                    return;
+                }
+
+                Register += int.Parse(Program[InstructionPointer].Split(" ")[1]);
+                MidOperation = false;
+            }
+        }
+    }
+}
