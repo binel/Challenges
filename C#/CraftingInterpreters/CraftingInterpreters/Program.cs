d97f5764cfa4bf3fdd12bde3c3b80c204d0b2828
Ben Nelson <binelson13@gmail.com>
2023-04-05 22:56:06 -0500

    Making progress on crafting interpreters and deciding on a challenge

--- C#/CraftingInterpreters/CraftingInterpreters/Program.cs
+++ C#/CraftingInterpreters/CraftingInterpreters/Program.cs
@@ -1,21 +1,39 @@
 ﻿namespace CraftingInterpreters
 {
-    public class Program
+  /*
+   * Created by working through "Crafting Interpreters" by Robert Nystrom.
+   * 
+   * I made some modifications so that programs would never require 
+   * pressing the shift key. (Although you do need a num pad to pull that 
+   * off) 
+   * 
+   * 
+   * 
+   */
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Enter program.");
-                return;
-            }
-
-            Run(args[0]);   
-        }
-
-        private static void Run(string source)
-        {
-
-        }
+      Prompt();
     }
+
+    private static void Prompt()
+    {
+      while (true)
+      {
+        Console.Write(">");
+        var line = Console.ReadLine();
+        if (line == null) break;
+        Run(line);
+      }
+    }
+
+    private static void Run(string source)
+    {
+      Scanner s = new Scanner(source);
+      var tokens = s.Scan();
+
+      Console.WriteLine(string.Join(',', tokens));
+    }
+  }
 }
---
+++ C#/CraftingInterpreters/CraftingInterpreters/Scanner.cs
@@ -0,0 +1,131 @@
+﻿using TokenType = CraftingInterpreters.Token.TokenType;
+
+namespace CraftingInterpreters
+{
+  public class Scanner
+  {
+    private readonly string _source;
+    private readonly List<Token> _tokens = new List<Token>();
+
+    // First char in lexeme being scanned 
+    private int _start = 0;
+    // position in lexeme 
+    private int _position = 0;
+    // what source line position is on
+    private int _line = 1;
+
+    public Scanner(string source)
+    {
+      _source = source;
+    }
+
+    public List<Token> Scan()
+    {
+      while (!AtEnd())
+      {
+        _start = _position;
+        ScanToken();
+      }
+
+      _tokens.Add(new Token(Token.TokenType.EOF, "", null, _line));
+      return _tokens;
+    }
+
+    private void ScanToken()
+    {
+      char c = Advance();
+      switch (c)
+      {
+        case '[': AddToken(TokenType.LEFT_BRACKET); break;
+        case ']': AddToken(TokenType.RIGHT_BRACKET); break;
+        case ',': AddToken(TokenType.COMMA); break;
+        case '.': AddToken(TokenType.DOT); break;
+        case '-': AddToken(TokenType.MINUS); break;
+        case '+': AddToken(TokenType.PLUS); break;
+        case ';': AddToken(TokenType.SEMICOLON); break;
+        case '*': AddToken(TokenType.STAR); break;
+        case '=':
+          AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+          break;
+        case '/':
+          if (Match('/'))
+          {
+            while (Peek() != '\n' && !AtEnd()) Advance();
+          }
+          else
+          {
+            AddToken(TokenType.SLASH);
+          }
+          break;
+        case ' ':
+        case '\r':
+        case '\t':
+          break;
+        case '\n':
+          _line++;
+          break;
+        case '\'': String(); break;
+        default:
+          Error.Report(_line, $"Unexpected character {c}");
+          break;
+      }
+    }
+
+    private void AddToken(Token.TokenType type)
+    {
+      AddToken(type, null);
+    }
+
+    private void AddToken(Token.TokenType type, object literal)
+    {
+      var lexeme = _source.Substring(_start, _position - _start);
+      var t = new Token(type, lexeme, literal, _line);
+      _tokens.Add(t);
+    }
+
+    private bool AtEnd()
+    {
+      return _position >= _source.Length;
+    }
+
+    private bool Match(char expected)
+    {
+      if (AtEnd()) return false;
+      if (_source[_position] != expected) return false;
+
+      _position++;
+      return true;
+    }
+
+    private char Peek()
+    {
+      if (AtEnd()) return '\0';
+      return _source[_position];
+    }
+
+    private char Advance()
+    {
+      return _source[_position++];
+    }
+
+    private void String()
+    {
+      while (Peek() != '\'' && !AtEnd())
+      {
+        if (Peek() == '\n') _line++;
+        Advance();
+      }
+
+      if (AtEnd())
+      {
+        Error.Report(_line, "Unterminated string");
+        return;
+      }
+
+      Advance();
+
+      var value = _source.Substring(_start + 1, _position - _start - 2);
+      AddToken(TokenType.STRING, value);
+    }
+  }
+}
