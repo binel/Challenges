644a0d75668d1b5f6ec85538c1a2b98ce445a2f9
Ben Nelson <binelson13@gmail.com>
2023-04-06 23:19:01 -0500

    Crafting interpreters scanner chapter

--- C#/CraftingInterpreters/CraftingInterpreters/Scanner.cs
+++ C#/CraftingInterpreters/CraftingInterpreters/Scanner.cs
@@ -4,6 +4,28 @@ namespace CraftingInterpreters
 {
   public class Scanner
   {
+    private static readonly Dictionary<string, TokenType> _keywords =
+      new Dictionary<string, TokenType>()
+      {
+        {"and", TokenType.AND },
+        {"class", TokenType.CLASS },
+        {"else", TokenType.ELSE },
+        {"false", TokenType.ELSE },
+        {"for", TokenType.FOR },
+        {"fun", TokenType.FUN },
+        {"if", TokenType.IF },
+        {"nil", TokenType.NIL },
+        {"or", TokenType.OR },
+        {"print", TokenType.PRINT },
+        {"return", TokenType.RETURN },
+        {"super", TokenType.SUPER },
+        {"this", TokenType.THIS },
+        {"true", TokenType.TRUE },
+        {"var", TokenType.VAR },
+        {"while", TokenType.WHILE }
+      };
+
+
     private readonly string _source;
     private readonly List<Token> _tokens = new List<Token>();
 
@@ -66,7 +88,18 @@ namespace CraftingInterpreters
           break;
         case '\'': String(); break;
         default:
-          Error.Report(_line, $"Unexpected character {c}");
+          if (IsDigit(c))
+          {
+            Number();
+          }
+          else if  (IsAlpha(c))
+          {
+            Identifier();
+          }
+          else 
+          {
+            Error.Report(_line, $"Unexpected character {c}");
+          }
           break;
       }
     }
@@ -103,6 +136,12 @@ namespace CraftingInterpreters
       return _source[_position];
     }
 
+    private char PeekNext()
+    {
+      if (_position + 1 >= _source.Length) return '\0';
+      return _source[_position + 1];
+    }
+
     private char Advance()
     {
       return _source[_position++];
@@ -126,6 +165,51 @@ namespace CraftingInterpreters
 
       var value = _source.Substring(_start + 1, _position - _start - 2);
       AddToken(TokenType.STRING, value);
+    }
+
+    private void Number()
+    {
+      while (IsDigit(Peek())) Advance();
+
+      if (Peek() == '.' && IsDigit(PeekNext())) {
+        Advance();
+      }
+
+      while (IsDigit(Peek())) Advance();
+
+      AddToken(TokenType.NUMBER, double.Parse(_source.Substring(_start, _position - _start)));
+    }
+
+    private void Identifier()
+    {
+      while (IsAlphaNumeric(Peek())) Advance();
+
+      var text = _source.Substring(_start, _position - _start);
+      if (_keywords.ContainsKey(text))
+      {
+        AddToken(_keywords[text]);
+      }
+      else
+      {
+        AddToken(TokenType.IDENTIFIER);
+      }
+    }
+
+    private bool IsDigit(char c)
+    {
+      return c >= '0' && c <= '9';
+    }
+
+    private bool IsAlpha(char c)
+    {
+      return (c >= 'a' && c <= 'z') ||
+             (c >= 'A' && c <= 'Z') ||
+             c == '_';
+    }
+
+    private bool IsAlphaNumeric(char c)
+    {
+      return IsAlpha(c) || IsDigit(c);
     }
   }
 }
